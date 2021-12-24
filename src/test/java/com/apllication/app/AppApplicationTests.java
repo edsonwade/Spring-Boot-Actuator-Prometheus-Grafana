6293d1bf0be4dba14825064bd62af07f2da6d908
vanil <vanil@DESKTOP-EPO41LJ>
2021-12-24 16:54:21 +0000

    introduction  to spring boot

---
+++ src/test/java/com/apllication/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apllication.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
