007256a1f65649bf6e5d15dc3f3d77ba1ca63193
vanil <vanil@DESKTOP-EPO41LJ>
2021-12-26 19:34:02 +0000

    Exception handling - update

--- src/main/java/com/apllication/app/service/UserService.java
+++ src/main/java/com/apllication/app/service/UserService.java
@@ -10,6 +10,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
@@ -60,10 +61,15 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = userRepo.getOne(id);
-        updateData(entity, obj);
-        return userRepo.save(entity);
-    }
+        try {
+            User entity = userRepo.getOne(id);
+            updateData(entity, obj);
+            return userRepo.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+
+    }''
 
     /**
      * atualize os  dados do usuário
